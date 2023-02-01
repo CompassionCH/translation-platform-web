04a74067ee586ca1a7395fb55588565d5619b391
Guillaume Hochet <guillaume.hochet@ik.me>
2023-02-01 08:47:44 +0100

    fix xmlrpc error in build

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,17 @@
  * styles and loads icons
  */
 import { mount } from "@odoo/owl";
-import { Buffer } from 'buffer';
-import Layout from './pages/Layout';
-import translateFn from './i18n';
-import './style.css';
-import './icons';
+import { Buffer } from "buffer";
+import Layout from "./pages/Layout";
+import translateFn from "./i18n";
+import "./style.css";
+import "./icons";
 
 // Buffer Polyfill used by the xml-rpc library
+window.Buffer = Buffer;
 globalThis.Buffer = Buffer;
 
 mount(Layout, document.body, {
   translateFn,
-  dev: true,
+  dev: import.meta.env.DEV,
 });
--- vite.config.js
+++ vite.config.js
@@ -1,47 +1,52 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig, loadEnv } from "vite";
 
 /**
  * We define a small home made Vite plugin to be able to load xml file
  * as Odoo template strings. It is thus possible to do the following:
- * 
+ *
  * import template from './template.xml';
  * const X extends Component {
  *  static template = template;
  * }
  */
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), "");
 
   return {
     base: env.SERVE_URL,
+    build: {
+      commonjsOptions: {
+        ignoreTryCatch: (id) => id !== "stream",
+      },
+    },
     resolve: {
       alias: {
-        stream: 'stream-browserify',
-        events: 'events',
+        stream: "stream-browserify",
+        events: "events",
       },
     },
     optimizeDeps: {
       esbuildOptions: {
         define: {
-          global: 'globalThis',
+          global: "globalThis",
         },
       },
     },
     plugins: [
       {
-        name: 'vite-template-plugin',
+        name: "vite-template-plugin",
         transform(src, id) {
-          if (id.endsWith('.xml')) {
+          if (id.endsWith(".xml")) {
             return {
               map: null,
               code: `
               import { xml } from "@odoo/owl";
               export default ${"xml`" + src + "`"};
               `,
-            }
+            };
           }
-        }
-      }
-    ]
+        },
+      },
+    ],
   };
 });
