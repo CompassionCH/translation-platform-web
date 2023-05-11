c4e7f3366e9c1526262764d6b89981507e209b61
NoeBerdoz <berdoznoe@gmail.com>
2023-05-11 16:30:50 +0200

    Update Translations for Child Protection

--- src/components/ChildModal.ts
+++ src/components/ChildModal.ts
@@ -18,32 +18,67 @@ class ChildModal extends Component {
           <div class="flex flex-col mb-3">
             <p class="text-sm font-semibold text-slate-700 mb-2">Expected/acceptable behaviors:</p>
             <ul class="list-disc ml-4 mb-4 text-sm">
-              <li class="mb-2">I will demonstrate appropriate respect and dignity towards all children, and I will show Jesus' love and care for them, regardless of their gender, age, race, religion, social background, culture, social needs, or disabilities.</li>
-              <li class="mb-2">I will respond to appropriate and reasonable expectations for children based on their age and level of maturity. (For example, it is normal, in terms of development, for a three-year-old to become easily upset when tired, but one can expect a teenager to control their emotions even when very tired.)</li>
-              <li class="mb-2">I will communicate with children in a way that is appropriate for their age.</li>
-              <li class="mb-2">I will only participate in activities with children in open and visible places, and in the event that an activity needs to be practiced in an enclosed space, I will ensure that at least one representative from Compassion is present.</li>
-              <li class="mb-2">If I witness abuse of a child, if I know that a child is in danger, if I notice concerning behavior from colleagues, partners, or other representatives, or if a child approaches me to report abuse, I will take it seriously and report it to the relevant personnel or authorities. I will seek to do everything in my power to ensure that the child is out of harm's way.</li>
-              <li class="mb-2">I will maintain the confidentiality of all information related to child protection investigations, keeping in mind everyone's concerns regarding privacy and dignity.</li>
-              <li class="mb-2">If necessary, I will comply with child protection investigations and provide all necessary documents or information for the proper conduct of the investigation.</li>
-              <li class="mb-2">I will contribute to creating an environment where children are respected and encouraged to speak up about their concerns and rights.</li>
-              <li class="mb-2">I will respect Compassion's rules regarding communication with children, including interactions through social media.</li>
-              <li class="mb-2">I will adhere to the standards of dignity (for example, I will only photograph or film children who are dressed appropriately, who understand that they are being filmed, etc.) in photography and videography.</li>
-              <li class="mb-2">I will take care of my appearance, language, and actions to ensure that my behavior reflects my respect for children and their families, their culture, and their rights, and complies with the provided recommendations.</li>
+              <li class="mb-2">I will demonstrate the proper respect and dignity of all children and will demonstrate Jesus’s love
+              and care for them, regardless of their gender, age, race, religion, social background, culture, special
+              need or disability.</li>
+              <li class="mb-2">I will maintain appropriate and reasonable expectations for children based on their age and ability
+              level. (For example, it is developmentally normal for a three-year-old to become upset easily when
+              they are tired but we could expect a teenager to manage their emotions even when they are very
+              tired.)</li>
+              <li class="mb-2">I will engage in age-appropriate communication with beneficiaries.</li>
+              <li class="mb-2">I will submit to the appropriate background or police checks as permissible by law prior to face-
+              to-face contact with beneficiaries (at minimum – an employee may be requested to comply with background/police checks at hire).</li>
+              <li class="mb-2">I will engage in activities with beneficiaries only in open or visible places, and in the event that an
+              activity needs to take place in an enclosed space, I will ensure that at least one other approved adult
+              is present.</li>
+              <li class="mb-2">If I witness child abuse, know a child is in danger, observe any concerning behaviors from colleagues,
+              partners or other representatives, or a child comes to me with a report of abuse, I will take it seriously
+              and report it to the proper staff or relevant authorities. I will seek to do everything within my power
+              to ensure the child is out of danger.</li>
+              <li class="mb-2">I will keep all information about child protection investigations confidential, keeping in mind privacy
+              and dignity concerns of all involved.</li>
+              <li class="mb-2">If requested, I will comply with child protection related investigations and make available any
+              documentation or other information necessary for the completion of the investigation.</li>
+              <li class="mb-2">I will contribute to building an environment where children are respected and encouraged to discuss
+              their concerns and rights.</li>
+              <li class="mb-2">I will follow Compassion’s rules about communication with beneficiaries, including social media
+              interaction.</li>
+              <li class="mb-2">I will follow dignity standards (for example, only recording children who are dressed appropriately,
+                understand they are being recorded, etc.) regarding child photography and videography.</li>
+              <li class="mb-2">I will be careful about my appearance, language, action, to ensure that my behavior demonstrates
+              a respect for beneficiaries and their families, their culture and their rights and follow
+              recommendations provided.</li>
             </ul>
           </div>
           <div class="flex flex-col mb-3">
             <p class="text-sm font-semibold text-slate-700 mb-2">Unnaceptable behaviors:</p>
             <ul class="list-disc ml-4 mb-4 text-sm">
-              <li class="mb-2">I will not solicit any romantic/intimate relationship and will never engage in sexual/sexually suggestive behavior with a child, regardless of their age.</li>
-              <li class="mb-2">I will never engage in sexual/sexually suggestive behavior with a child under the age of 18, regardless of the age of majority in this country.</li>
-              <li class="mb-2">I will never speak in a verbally/emotionally abusive, sexually suggestive, degrading, humiliating, stigmatizing, or culturally inappropriate manner with a child.</li>
-              <li class="mb-2">I will not touch (such as kissing, hugging, lifting, holding, etc.) children in an inappropriate or culturally insensitive manner.</li>
-              <li class="mb-2">I will never resort to any type of physical punishment or corporal punishment (such as whipping, caning, slapping, punitive physical exercises, and other types of corporal punishment) as a disciplinary method for children.</li>
-              <li class="mb-2">I will never travel alone with a child without an approved representative or prior approval, except in cases of vital emergency where circumstances require immediate travel.</li>
-              <li class="mb-2">I will not employ any child in any form of child labor and will respect local laws regarding the employment of children.</li>
-              <li class="mb-2">I will never visit a child, their parent/guardian, and/or their church outside of acceptable program standards regarding visits or tours.</li>
-              <li class="mb-2">I will not collect, disclose, or assist in the disclosure, without prior express permission, of information about children or their families containing private or confidential information such as physical, mental, or emotional health status, financial data, history of abuse or exploitation, or any representation that is not in accordance with the dignity standards identified by Compassion. Additionally, I will not disclose information that could allow for the geographic location of a child or their family to be identified (such as a map indicating the location of their home, the address of their home, or a geolocation tag of their home contained in a photo). I understand that disclosure also applies to content published in paper or digital form, on any public or private platform.</li>
-              <li class="mb-2">I will not authorize or participate in any illegal, dangerous, or abusive behavior towards any child, whether it involves exploitation, trafficking, harmful traditional practices, or spiritual or ritual abuse.</li>
+              <li class="mb-2">I will not solicit a romantic/dating relationship and will never engage in sexual/sexually suggestive
+              behavior with any beneficiary, regardless of age.</li>
+              <li class="mb-2">I will never engage in sexual/sexually suggestive behavior with any child under age 18, regardless of
+              the legal age of consent in-country.</li>
+              <li class="mb-2">I will never use language that is verbally/emotionally abusive, sexually suggestive, degrading,
+              humiliating, shaming or is otherwise culturally inappropriate with a beneficiary.</li>
+              <li class="mb-2">I will not touch (some examples include kissing, cuddling, picking up, holding, etc.) beneficiaries in
+              an inappropriate or culturally insensitive way.</li>
+              <li class="mb-2">I will never use any kind of physical discipline or physical punishment (some examples include
+                whipping, caning, slapping, forcing punitive exercise, and other types of corporal punishment) as a
+                method of correction for beneficiaries.</li>
+              <li class="mb-2">I will never travel alone with a beneficiary, without an approved representative or prior approval,
+              except in a life-threatening emergency where circumstances require immediate movement.</li>
+              <li class="mb-2">I will not hire any child in any harmful form of child labor and follow local laws regarding child
+              employment.</li>
+              <li class="mb-2">I will never visit a beneficiary, their caregiver(s)and/or church outside the bounds of acceptable
+              program or tours and visits standards.</li>
+              <li class="mb-2">I will not gather, disclose or support the disclosure of information about beneficiaries or their
+              families without prior, express permission that contains private or privileged content such as
+              physical, mental or emotional health status, financial data, history of abuse or exploitation, or any
+              portrayal that does not meet Compassion’s identified dignity standards. Additionally, I will not
+              disclose information that might allow a beneficiary or their family to be physically located (such as
+              a map to their home, home address, or geo-tag to their location in a photograph.) I understand that
+              disclosure includes content published in any print or digital form on any public or private platform.</li>
+              <li class="mb-2">I will not condone or participate in illegal, unsafe or abusive behavior of any child, including
+              exploitation, trafficking, harmful traditional practices, and spiritual or ritualistic abuse.</li>
             </ul>
           </div>
         </div>
