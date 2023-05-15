0b33666bb7b83a2920f115ba3762c4a4886ccfe7
NoeBerdoz <44669187+NoeBerdoz@users.noreply.github.com>
2023-05-15 13:05:23 +0200

    Update fr_CH.ts (POEditor.com)

--- src/i18n/fr_CH.ts
+++ src/i18n/fr_CH.ts
@@ -1,11 +1,10 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="fr">
-  <context>
-    <name></name>
-    <message>
-      <source>Good morning</source>
-      <translation>Bonjour</translation>
-    </message>
-  </context>
-</TS>
+[
+    {
+        "term": "Good morning",
+        "definition": "Bonjour",
+        "context": "",
+        "term_plural": "",
+        "reference": "",
+        "comment": ""
+    }
+]
