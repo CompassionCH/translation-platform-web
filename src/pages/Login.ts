5b15b26d350a1a8099efbec124064c4b9b5be989
Clément <clement.charmillot@gmail.com>
2024-07-22 15:40:56 +0200

    chore: format file and cleanup html

--- src/pages/Login.ts
+++ src/pages/Login.ts
@@ -25,18 +25,18 @@ class Login extends Component {
             <span class="relative">
               <input t-ref="password" class="compassion-input text-sm mb-3 !pr-[30px]" t-att-type="state.showPassword ? 'text' : 'password'" placeholder="Password" t-model="state.password" />
               <button t-on-click="togglePassword" type="button">
-              <svg class="shrink-0 size-3.5 absolute right-2 top-0" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#666" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <g t-if="!state.showPassword">
-                  <path d="M9.88 9.88a3 3 0 1 0 4.24 4.24"></path>
-                  <path d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68"></path>
-                  <path d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61"></path>
-                  <line x1="2" x2="22" y1="2" y2="22"></line>
-                </g>
-                <g t-else="">
-                  <path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"></path>
-                  <circle cx="12" cy="12" r="3"></circle>
-                </g>
-              </svg>
+                <svg class="shrink-0 size-3.5 absolute right-2 top-0" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="#778" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                  <g t-if="state.showPassword">
+                    <path d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z" />
+                    <circle cx="12" cy="12" r="3" />
+                  </g>
+                  <g t-else="">
+                    <path d="M9.88 9.88a3 3 0 1 0 4.24 4.24" />
+                    <path d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68" />
+                    <path d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61" />
+                    <line x1="2" x2="22" y1="2" y2="22" />
+                  </g>
+                </svg>
               </button>
             </span>
             <Button color="'compassion'" class="'w-full mb-2'" size="'sm'">Login</Button>
